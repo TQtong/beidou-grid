9db078922659d20cf6752d3c209448f28432497d
TQ <tangqi@bwton.com>
2026-05-26 17:13:38 +0800

    chore: update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/beidou-grid/',
+  base: './',
   plugins: [vue(), cesium()],
   resolve: {
     alias: {
